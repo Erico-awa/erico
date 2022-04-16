6fe3bb10d4d8efa3fe24da837b9737dbc3f59776
Ericoawa <100107874+Erico-awa@users.noreply.github.com>
2022-04-16 19:20:02 +0800

    Add files via upload

--- FILES/GDGYX/static/index.js
+++ FILES/GDGYX/static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://github.com/Erico-awa/erico/blob/main/FILES/GDGYX/static/click.png?raw=true';
+var url = 'https://github.com/Erico-awa/erico/blob/main/FILES/GDGYX/static/click.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
